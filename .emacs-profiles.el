c7b32aaaf19134a41438c2a283153c4986e6c6b3
ezzobirbezziou <ezzobir_bezziou@protonmail.com>
2024-02-18 10:44:39 +0100

    some chemacs2 configuration

---
+++ .emacs-profiles.el
@@ -0,0 +1,3 @@
+(("myconfig" . ((user-emacs-directory . "~/my_files/.dotfiles/.config/emacs-configs/myconfig")))
+ ("spacemacs" . ((user-emacs-directory . "~/my_files/.dotfiles/.config/emacs-configs/spacemacs"))))
+
